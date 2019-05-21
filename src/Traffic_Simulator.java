65a493141c11ae1fc5e843e656211fe95c3e0220
tristansimonson <tristan-simonson@uiowa.edu>
2019-05-21 15:01:06 -0500

    added move function to decide where vehicles should go

--- src/Traffic_Simulator.java
+++ src/Traffic_Simulator.java
@@ -12,7 +12,31 @@ public class Traffic_Simulator {
 	// # x # # ex address 2 3 
 	// # # # #
 	// # # # #
-	public void move() {
+	public static boolean move(Vehicle v) {
 		// check location of vehicle and compare it to destination, depending on cardinal direction have them move respectively
+		Address target = v.destination;
+		Address loc = v.location;
+		if(loc.street1 != target.street1) {
+			if(loc.street1 > target.street1) {
+				v.location.street1 -= 1;
+				return false;
+			}
+			else {
+				v.location.street1 += 1;
+				return false;
+			}
+		}
+		if(loc.street2 != target.street2) {
+			if(loc.street2 > target.street2) {
+				v.location.street2 -= 1;
+				return false;
+			}
+			else {
+				v.location.street2 += 1;
+				return false;
+			}
+		}
+		System.out.println("Vehicle has arrived at destination");
+		return true;
 	}
 }
