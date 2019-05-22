31e61c4343533b707d86dd7727101bd2b1a30093
tristansimonson <tristan-simonson@uiowa.edu>
2019-05-21 19:02:55 -0500

    moved bounds check into move function

--- src/Traffic_Simulator.java
+++ src/Traffic_Simulator.java
@@ -28,8 +28,7 @@ public class Traffic_Simulator {
 	
 	// runs simulation
 	public static void run(ArrayList<Vehicle> v, ArrayList<Stoplight> s, int[] map, int timer) {
-		// increment timer and return if reaches zero
-		// if vehicle list empty return
+		// if vehicle list empty or timer ran out return
 		if(v.isEmpty() || timer == 0) {
 			return;
 		}
@@ -37,18 +36,23 @@ public class Traffic_Simulator {
 		for(int i = 0; i < v.size(); i++) {
 			// check if there is a stoplight at the node
 			// TODO: check if vehicle is in a queue
+			// TODO: should move go here?
 			for(int j = 0; j < s.size(); j++) {
 				// car has arrived at stoplight
 				if (s.get(j).EWStreet == v.get(i).location.street1 && s.get(j).NSStreet == v.get(i).location.street2) {
 					// check if light is green or yellow
+					// TODO: need to add behavior for driver types later
+					// TODO: need to make moves in order of stoplight queue
 					if(s.get(j).color != LightColor.GREEN) {
-						// make sure move is within map dimensions
+					}
+					if(s.get(j).color == LightColor.RED) {
 						// if light is red car will be added to queue of light
+						s.get(j).queue.add(v.get(i));
 					}
 				}
-				// no stoplight at car 
+				// no stoplight at vehicle location 
 				else {
-					// continue
+					// TODO: should I do a move here for recursive call?
 				}
 			}
 			// if car reaches destination then remove from list
@@ -65,31 +69,37 @@ public class Traffic_Simulator {
 	// # x # # ex address 2 3 
 	// # # # #
 	// # # # #
-	public static boolean move(Vehicle v) {
+	public static boolean move(Vehicle v, int[] map) {
 		// check location of vehicle and compare it to destination, depending on cardinal direction have them move respectively
 		Address target = v.destination;
 		Address loc = v.location;
+		boolean ret = true;
 		if(loc.street1 != target.street1) {
 			if(loc.street1 > target.street1) {
 				v.location.street1 -= 1;
-				return false;
+				ret = false;
 			}
 			else {
 				v.location.street1 += 1;
-				return false;
+				ret = false;
 			}
 		}
-		if(loc.street2 != target.street2) {
+		else if(loc.street2 != target.street2) {
 			if(loc.street2 > target.street2) {
 				v.location.street2 -= 1;
-				return false;
+				ret = false;
 			}
 			else {
 				v.location.street2 += 1;
-				return false;
+				ret = false;
 			}
 		}
-		System.out.println("Vehicle has arrived at destination");
-		return true;
+		if(v.location.street1 > map[0] || v.location.street2 > map[1] || v.location.street1 < 0) {
+			System.out.println("Out of bounds for map");
+		}
+		if(ret == true) {
+			System.out.println("Vehicle has arrived at destination");
+		}
+		return ret;
 	}
 }
